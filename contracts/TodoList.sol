007fccb6b004c275b17743cee43ac4f6af4b805c
SharmilaS22 <sharmilasureshkumar22@gmail.com>
2021-03-20 00:10:51 +0530

    serve client files

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -2,7 +2,25 @@
 pragma solidity >=0.4.22;
 
 contract TodoList {
+
     // state variable - written to blockchain
     uint public taskCount = 0;
     
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+    }
+
+    mapping(uint => Task) public tasks;
+
+    constructor() public {
+        createTask("Learn blockchain");
+    }
+
+    function createTask(string memory _content) public {
+        taskCount ++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
+
 }
